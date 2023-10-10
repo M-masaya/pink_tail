8cae58518f51b7679de9545068526b49a5da0ad5
201081 <201081@WINDOWS-RDK3N1B.mr.hi-joho.ac.jp>
2023-10-10 11:27:36 +0900

    1

--- src/servlet/SampleServlet.java
+++ src/servlet/SampleServlet.java
@@ -37,7 +37,7 @@ public class SampleServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		//test2
+		//test3
 	}
 
 }
